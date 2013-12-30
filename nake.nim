fc56c3fe78219fbf0aee2bd7464770c01cb788b8
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2013-12-30 20:06:25 +0100

    Cleans up trailing whitespace.

--- nake.nim
+++ nake.nim
@@ -3,7 +3,7 @@ DO AS THOU WILST PUBLIC LICENSE
 
 Whoever should stumble upon this document is henceforth and forever
 entitled to DO AS THOU WILST with aforementioned document and the
-contents thereof. 
+contents thereof.
 
 As said in the Olde Country, `Keepe it Gangster'."""
 
@@ -18,8 +18,8 @@ type
   PTask* = ref object
     desc*: string
     action*: TTaskFunction
-  TTaskFunction* = proc() 
-var 
+  TTaskFunction* = proc()
+var
   tasks = initTable[string, PTask](32)
   careful = false
 
