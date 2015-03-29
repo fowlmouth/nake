303b489b30fb5bc2b0f4b8467ae0e85da68f17a3
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2015-03-29 21:20:58 +0200

    Adds website maintenance nakefile tasks. Refs #27.

--- nakefile.nim
+++ nakefile.nim
@@ -11,7 +11,7 @@ when defined(Linux):
     echo "Symlinked file"
 
 
-task "docs", "generate user documentation for nake API and local rst files":
+proc task_docs() =
   if "nake.html".needsRefresh("nake.nim"):
     echo "nake.nim -> nake.html"
     direShell nimExe, "doc2", "--verbosity:0", "--index:on", "nake.nim"
@@ -26,9 +26,8 @@ task "docs", "generate user documentation for nake API and local rst files":
       echo rstSrc, " -> ", rstDest
 
   direShell nimExe, "buildIndex ."
-  echo "Finished generating docs"
 
-task "install", "compile and install nake binary":
+proc task_install() =
   direShell nimExe, "c", "nake"
 
   var
@@ -63,5 +62,69 @@ task "install", "compile and install nake binary":
     quit 1
 
   installMethod "nake", path[index]/"nake"
+
+
+proc switchToGhPages(iniPathOrDir = ".") =
+  ## Forces changing git branch to ``gh-pages`` and running
+  ## ``gh_nimrod_doc_pages``.
+  ##
+  ## **This is a potentially destructive action!**. Pass the directory where
+  ## the ``gh_nimrod_doc_pages.ini`` file lives, or the path to the specific
+  ## file if you renamed it.
+  assert(not iniPathOrDir.isNil)
+  let
+    ghExe = findExe("gh_nimrod_doc_pages")
+    gitExe = findExe("git")
+
+  if ghExe.len < 1:
+    quit("Could not find gh_nimrod_doc_pages binary in $PATH, aborting")
+
+  if gitExe.len < 1:
+    quit("Could not find git binary in $PATH, aborting")
+
+  echo "Changing branches to render gh-pages…"
+  let
+    nakeExe = "nakefile".addFileExt(ExeExt)
+    ourselves = readFile(nakeExe)
+  direShell gitExe & " checkout gh-pages"
+  # Keep ingored files http://stackoverflow.com/a/3801554/172690.
+  when defined(posix):
+    shell "rm -Rf `git ls-files --others --exclude-standard`"
+  removeDir("gh_docs")
+  direShell ghExe & " -c " & iniPathOrDir
+  writeFile(nakeExe, ourselves)
+  direShell "chmod 775 nakefile"
+  echo "All commands run, now check the output and commit to git."
+  when defined(macosx):
+    shell "open index.html"
+  echo "Wen you are done come back with './" & nakeExe & " postweb'."
+
+
+proc switchBackFromGhPages() =
+  ## Counterpart of ``switchToGhPages``.
+  let gitExe = findExe("git")
+  if gitExe.len < 1:
+    quit("Could not find git binary in $PATH, aborting")
+
+  echo "Forcing changes back to master."
+  direShell gitExe & " checkout -f @{-1}"
+  echo "Updating submodules just in case."
+  direShell gitExe & " submodule update"
+  removeDir("gh_docs")
+
+
+task "docs", "generate user documentation for nake API and local rst files":
+  task_docs()
+  echo "Finished generating docs"
+
+task "install", "compile and install nake binary":
+  task_install()
   echo "Great success!"
 
+task "web", "switches to gh-pages branch and runs gh_nimrod_doc_pages":
+  switchToGhPages("web.ini")
+  echo "Now you can run 'git add .' if everything looks good."
+
+task "postweb", "switches back from gh-pages":
+  switchBackFromGhPages()
+  echo "Back from gh-pages"
